5dec42a69c3e6e1a0d1a4580d9ba17a5f67f1e9c
Mario Domi <mariodomi@tuta.io>
2023-09-14 02:10:11 +0200

    Initial commit

---
+++ test/gsm-cdk.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const GsmCdk = require('../lib/gsm-cdk-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/gsm-cdk-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new GsmCdk.GsmCdkStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
